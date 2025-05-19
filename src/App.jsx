20ed1855ac4d6782e7c85f715287d4e477305425
begliado <dodo09.97@hotmail.fr>
2025-05-19 19:45:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+// src/App.jsx
+
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 import Home from './pages/Home';
+import MJPage from './pages/MJ';
+import GamePage from './pages/Game';
+import CharacterPage from './pages/Character';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* tes autres routes (MJ, game, character) */}
+        <Route path="/mj" element={<MJPage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/character/:id" element={<CharacterPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
