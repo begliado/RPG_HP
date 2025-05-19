ee2c8f0f35fd7e2a16b2a7342d89a4438a86a1f1
begliado <dodo09.97@hotmail.fr>
2025-05-19 19:06:38 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold">Hogwarts RPG</h1>
-      <p>Bienvenue ! Prochaine étape : authentification et routage.</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* tes autres routes (MJ, game, character) */}
+      </Routes>
+    </Router>
   );
 }
