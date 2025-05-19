69cb06f0f37eb5c2cfdd203556f2744f31921506
begliado <dodo09.97@hotmail.fr>
2025-05-19 22:23:15 +0200

    Update Game.jsx

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,22 +1,22 @@
 // src/pages/Game.jsx
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function GamePage() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Salon de jeu</h1>
-      <p>Bienvenue dans le lobby de jeu ! Choisissez une option :</p>
       <ul className="list-disc list-inside mt-2">
         <li>
-          <a href="/character/test-student" className="text-blue-600 underline">
+          <Link to="/character/test-student" className="text-blue-600 underline">
             Voir mon personnage test
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/mj" className="text-blue-600 underline">
+          <Link to="/mj" className="text-blue-600 underline">
             Dashboard MJ
-          </a>
+          </Link>
         </li>
       </ul>
     </div>
