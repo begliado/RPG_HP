edf44cfb21f02ea4bbf4a768a8587889c381b86d
begliado <dodo09.97@hotmail.fr>
2025-05-24 18:20:46 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
 // src/App.jsx
 
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate
-} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from './pages/Home';
 import MJPage from './pages/MJ';
 import GamePage from './pages/Game';
 import CharacterPage from './pages/Character';
+import CreateCharacter from './pages/CreateCharacter';
 
 export default function App() {
   return (
-    <Router basename="/RPG_HP/">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mj" element={<MJPage />} />
         <Route path="/game" element={<GamePage />} />
         <Route path="/character/:id" element={<CharacterPage />} />
+        <Route path="/create-character" element={<CreateCharacter />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
