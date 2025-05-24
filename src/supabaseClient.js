50ee6df4fb098d341f7ecc54b044734a7c0b4f7b
begliado <dodo09.97@hotmail.fr>
2025-05-24 20:38:43 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -7,11 +7,8 @@ const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
-    // détecte et stocke automatiquement le token issu de l’URL au redirect OAuth
     detectSessionInUrl: true,
-    // rafraîchit le token en tâche de fond
-    autoRefreshToken: true,
-    // stocke la session dans localStorage pour persister sur F5
     persistSession: true,
+    autoRefreshToken: true,
   },
 });
