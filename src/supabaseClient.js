58ac7ceb3ada2c2a626e7e55e2a444aa9507e2d7
begliado <dodo09.97@hotmail.fr>
2025-05-25 16:06:16 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -7,7 +7,6 @@ const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
-    flowType: 'implicit',      // force response_type=token
     detectSessionInUrl: true,  // auto-consume fragment
     persistSession: true,      // store in localStorage
     autoRefreshToken: true,    // refresh tokens
